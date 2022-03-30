a3dbfc67c0d28112aec6ae547fa237e831de7af4
Edward Reynolds <edward.reynolds@students.mq.edu.au>
2022-03-30 17:16:08 +1100

    Added code to schedule first job and quit

--- src/pre-compiled/Client.java
+++ src/pre-compiled/Client.java
@@ -6,6 +6,8 @@ import java.net.*;
 	public class Client {  
 	    	public static void main(String[] args) {  
 	    		try{      
+	    		String[] strings;
+	    		int iterate = 0;	
 	    		
 			Socket s=new Socket("127.0.0.1",50000);  
 			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -20,6 +22,20 @@ import java.net.*;
 			str = (String)in.readLine();
 			System.out.println(str);
 			dout.flush();
+			
+			dout.write(("REDY\n").getBytes());
+			str = (String)in.readLine();
+			strings = str.split(" ");
+			dout.write(("GETS Capable " + strings[4] + " " + strings[5] + " " + strings[6] + "\n").getBytes());
+			str = (String)in.readLine();
+			System.out.println(str);
+			dout.write(("OK\n").getBytes());
+			str = (String)in.readLine();
+			System.out.println(str);
+			dout.write(("OK\n").getBytes());
+			str = (String)in.readLine();
+			System.out.println(str);
+			dout.write(("SCHD " + 0 + " super-silk 0\n").getBytes());
 			
 			dout.write(("QUIT\n").getBytes());
 			dout.close();  
